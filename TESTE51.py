62afd82fe7605157668e75085ac0190d3481688d
Isa Dantas <157421767+isabelasdantas@users.noreply.github.com>
2024-02-02 19:09:29 -0300

    Add files via upload

---
+++ TESTE51.py
@@ -0,0 +1,6 @@
+primeiro_termo = int(input('Primeiro termo: '))
+razao = int(input('Razão: '))
+decimo = primeiro_termo + (10 - 1) * razao
+for c in range(primeiro_termo, decimo + razao, razao):
+    print('{} '.format(c), end='--> ')
+print('ACABOU!')
